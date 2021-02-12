8a1a9a52d7e119d691c27d6a5cb20bff26b81f36
MBP16 <jonathanwatson@MBP16s-MacBook-Pro.local>
2021-02-12 11:42:48 -0500

    formatted h1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import Items from "./components/Items/Items"
+import Items from "./components/Items/Items";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello world</h1>
-      <Items/>
+    <div>
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <h1 className="text-center">My Pantry!</h1>
+          </div>
+        </div>
+      </div>
+
+      <Items />
     </div>
   );
 }
