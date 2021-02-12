b095fd9869804c247c37c17a33fa3ebec30ca873
MBP16 <jonathanwatson@MBP16s-MacBook-Pro.local>
2021-02-12 11:30:46 -0500

    initial hard coded setup complete

---
+++ src/components/ItemRow/ItemRow.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ItemRow = (props) => {
+
+  return (
+    <tr>
+      <th scope="row">{props.id}</th>
+      <td>{props.name}</td>
+    </tr>
+  );
+};
+
+export default ItemRow;
---
+++ src/components/Items/Items.jsx
@@ -0,0 +1,98 @@
+import React, { Component } from "react";
+import ItemRow from "../ItemRow/ItemRow";
+
+class Items extends Component {
+  state = {
+    items: [
+      {
+        id: 1,
+        name: "Garlic Salt",
+        calorieCount: 0,
+      },
+      {
+        id: 2,
+        name: "Macaroni",
+        caloreCount: 100,
+      },
+      {
+        id: 3,
+        name: "Canned Pinto Beans",
+        calorieCount: 25,
+      },
+    ],
+    newItem: "",
+  };
+
+  handleInputChange = (e) => {
+    const { name, value } = e.target;
+
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    const newItem = {
+      id: this.state.items.length + 1,
+      name: this.state.newItem,
+    };
+
+    const itemsArray = [...this.state.items];
+    itemsArray.push(newItem);
+
+    this.setState({
+      items: itemsArray,
+      newItem: ""
+    });
+  };
+
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <table className="table">
+              <thead>
+                <tr>
+                  <th scope="col">#</th>
+                  <th scope="col">Name</th>
+                </tr>
+              </thead>
+              <tbody>
+                {this.state.items.map((item) => (
+                  <ItemRow {...item} key={item.id} />
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col">
+            <form onSubmit={this.handleSubmit}>
+              <div className="mb-3">
+                <label htmlFor="item" className="form-label">
+                  Add a new item to your pantry.
+                </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="item"
+                  value={this.state.newItem}
+                  name="newItem"
+                  onChange={this.handleInputChange}
+                />
+              </div>
+              <button type="submit" className="btn btn-primary">
+                Submit
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Items;
